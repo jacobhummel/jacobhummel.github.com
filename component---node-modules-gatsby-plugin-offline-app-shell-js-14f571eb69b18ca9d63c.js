3b5a08db2b768661079244a5d294c9802e627a86
Jacob Hummel <j4cobhummel@gmail.com>
2018-08-12 23:16:14 -0500

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-14f571eb69b18ca9d63c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{257:function(e,t,n){"use strict";var u=n(31);t.__esModule=!0,t.default=void 0;var o=u(n(27)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement("div",null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-14f571eb69b18ca9d63c.js.map
--- src/utils/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from 'typography'
-import noriegaTheme from 'typography-theme-noriega'
-
-const typography = new Typography(noriegaTheme)
-
-export default typography
