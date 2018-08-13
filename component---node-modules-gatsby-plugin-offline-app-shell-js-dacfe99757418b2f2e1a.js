7e6a06b21d5692e3424d204c473943c67baec6ea
Jacob Hummel <j4cobhummel@gmail.com>
2018-08-12 20:20:42 -0500

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-dacfe99757418b2f2e1a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{231:function(e,t,n){"use strict";var u=n(30);t.__esModule=!0,t.default=void 0;var o=u(n(27)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement("div",null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-dacfe99757418b2f2e1a.js.map
--- src/utils/globals.js
+++
@@ -1,8 +0,0 @@
-export default {
-  color: {
-    text: 'hsla(0,0%,0%,0.8)',
-    primary: '#333',
-    target: '#c00',
-    boogie: '#0062FF'
-  }
-}
